58e47e734beefb8f6ead277b232c8bd0778071a3
Darren Cocco <git@darren.cocco.id.au>
2017-10-12 11:50:27 +1100

    Changed db field type from char to text for config storage

---
+++ db/upgrade.php
@@ -0,0 +1,18 @@
+<?php
+function xmldb_local_advancedconfig_upgrade($oldversion) {
+    global $DB;
+
+    $dbman = $DB->get_manager();
+    if ($oldversion < 2017101100) {
+
+        // Changing type of field config on table local_advconf_config to text.
+        $table = new xmldb_table('local_advconf_config');
+        $field = new xmldb_field('config', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null, 'context');
+
+        // Launch change of type for field config.
+        $dbman->change_field_type($table, $field);
+
+        // Advancedconfig savepoint reached.
+        upgrade_plugin_savepoint(true, 2017101100, 'local', 'advancedconfig');
+    }
+}
